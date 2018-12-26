040f079e129cf0d3ff0dd50435496ce5020c27fb
bqlin <bqlins@163.com>
2018-12-26 10:21:48 +0800

    [GuidedTour][Protocols and Extensions] 完成翻译更新

--- GuidedTour_cn_20181225.playground/Pages/Protocols and Extensions.xcplaygroundpage/Contents.swift
+++ GuidedTour_cn_20181225.playground/Pages/Protocols and Extensions.xcplaygroundpage/Contents.swift
@@ -1,18 +1,18 @@
-//: ## Protocols and Extensions
+//: ## 协议和扩展（Protocols and Extensions）
 //:
-//: Use `protocol` to declare a protocol.
+//: 使用`protocol`来声明一个协议。
 //:
 protocol ExampleProtocol {
      var simpleDescription: String { get }
      mutating func adjust()
 }
 
-//: Classes, enumerations, and structs can all adopt protocols.
+//: 类、枚举和结构体都可以遵循协议。
 //:
 class SimpleClass: ExampleProtocol {
      var simpleDescription: String = "A very simple class."
      var anotherProperty: Int = 69105
-     func adjust() {
+     func adjust() { // 此处无需使用 mutating 修饰
           simpleDescription += "  Now 100% adjusted."
      }
 }
@@ -31,11 +31,24 @@ b.adjust()
 let bDescription = b.simpleDescription
 
 //: - Experiment:
-//: Add another requirement to `ExampleProtocol`. What changes do you need to make to `SimpleClass` and `SimpleStructure` so that they still conform to the protocol?
+//: 写一个遵循这个协议的枚举。
 //:
-//: Notice the use of the `mutating` keyword in the declaration of `SimpleStructure` to mark a method that modifies the structure. The declaration of `SimpleClass` doesn’t need any of its methods marked as mutating because methods on a class can always modify the class.
+enum SimpleEnum: Int, ExampleProtocol {
+	case a = 1, b, c
+	var simpleDescription: String {
+		get {
+			return "A simple enum"
+		}
+	}
+	func adjust() {
+		print("SimpleEnum adjust...")
+	}
+}
+SimpleEnum.b.adjust()
+
+//: 注意声明`SimpleStructure`时候`mutating`关键字用来标记一个会修改结构体的方法。`SimpleClass`的声明不需要标记任何方法，因为类中的方法通常可以修改类属性（类的性质）。
 //:
-//: Use `extension` to add functionality to an existing type, such as new methods and computed properties. You can use an extension to add protocol conformance to a type that is declared elsewhere, or even to a type that you imported from a library or framework.
+//: 使用`extension`来为现有的类型添加功能，比如新的方法和计算属性。你可以使用扩展在别处修改定义，甚至是从外部库或者框架引入的一个类型，使得这个类型遵循某个协议。
 //:
 extension Int: ExampleProtocol {
     var simpleDescription: String {
@@ -45,19 +58,34 @@ extension Int: ExampleProtocol {
         self += 42
     }
  }
-print(7.simpleDescription)
+7.simpleDescription
 
 //: - Experiment:
-//: Write an extension for the `Double` type that adds an `absoluteValue` property.
+//: 给`Double`类型写一个扩展，添加`absoluteValue`功能。
 //:
-//: You can use a protocol name just like any other named type—for example, to create a collection of objects that have different types but that all conform to a single protocol. When you work with values whose type is a protocol type, methods outside the protocol definition are not available.
+extension Double {
+	var absoluteValue: Double {
+		get {
+			if self < 0 {
+				return -self;
+			}
+			return self;
+		}
+	}
+}
+var absoluteDouble = (-3.14159).absoluteValue
+
+//: 你可以像使用其他命名类型一样使用协议名——例如，创建一个有不同类型但是都遵循一个协议的对象集合。当你处理类型是协议的值时，协议外定义的方法不可用。
 //:
 let protocolValue: ExampleProtocol = a
-print(protocolValue.simpleDescription)
-// print(protocolValue.anotherProperty)  // Uncomment to see the error
+//print(protocolValue.simpleDescription)
+// 虽然 a 实现了 anotherProperty 的接口，但 protocolValue 声明时是 ExampleProtocol 类型的，所以就只能用 ExampleProtocol 协议中定义的接口
+//print("a.anotherProperty: \(a.anotherProperty)")
+//print("protocolValue.anotherProperty: \(protocolValue.anotherProperty)")
 
-//: Even though the variable `protocolValue` has a runtime type of `SimpleClass`, the compiler treats it as the given type of `ExampleProtocol`. This means that you can’t accidentally access methods or properties that the class implements in addition to its protocol conformance.
+//: 即使`protocolValue`变量运行时的类型是`simpleClass`，编译器会把它的类型当做`ExampleProtocol`。这表示你不能调用类在它遵循的协议之外实现的方法或者属性。
 //:
+
 
 
 //: [Previous](@previous) | [Next](@next)
