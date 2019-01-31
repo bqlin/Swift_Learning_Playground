3d476a4741cc30a209b8c9186eb9ed826da46488
bqlin <bqlins@163.com>
2019-01-31 17:32:11 +0800

    [The Swift Programming Language][Deinitialization] 注释日志打印

--- The Swift Programming Language.playground/Pages/Deinitialization.xcplaygroundpage/Contents.swift
+++ The Swift Programming Language.playground/Pages/Deinitialization.xcplaygroundpage/Contents.swift
@@ -43,21 +43,21 @@ class Player {
 }
 
 var playerOne: Player? = Player(coins: 100)
-print("A new player has joined the game with \(playerOne!.coinsInPurse) coins")
+//print("A new player has joined the game with \(playerOne!.coinsInPurse) coins")
 // 打印 "A new player has joined the game with 100 coins"
-print("There are now \(Bank.coinsInBank) coins left in the bank")
+//print("There are now \(Bank.coinsInBank) coins left in the bank")
 // 打印 "There are now 9900 coins left in the bank"
 
 playerOne!.win(coins: 2_000)
-print("PlayerOne won 2000 coins & now has \(playerOne!.coinsInPurse) coins")
+//print("PlayerOne won 2000 coins & now has \(playerOne!.coinsInPurse) coins")
 // 打印 "PlayerOne won 2000 coins & now has 2100 coins"
-print("The bank now only has \(Bank.coinsInBank) coins left")
+//print("The bank now only has \(Bank.coinsInBank) coins left")
 // 打印 "The bank now only has 7900 coins left"
 
 playerOne = nil
-print("PlayerOne has left the game")
+//print("PlayerOne has left the game")
 // 打印 "PlayerOne has left the game"
-print("The bank now has \(Bank.coinsInBank) coins")
+//print("The bank now has \(Bank.coinsInBank) coins")
 // 打印 "The bank now has 10000 coins"
 
 
