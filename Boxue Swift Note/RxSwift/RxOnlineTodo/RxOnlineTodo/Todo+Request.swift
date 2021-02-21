8cf732e5fe7aae265c1a59c44a477a6dbda89c7f
bqlin <bqlins@163.com>
2021-02-21 22:48:29 +0800

    [RxOnlineTodo] 实现数据请求与展示

--- Boxue Swift Note/RxSwift/RxOnlineTodo/RxOnlineTodo/Todo+Request.swift
+++ Boxue Swift Note/RxSwift/RxOnlineTodo/RxOnlineTodo/Todo+Request.swift
@@ -8,30 +8,37 @@
 
 import Foundation
 import Alamofire
+import RxSwift
+
+enum GetTodoListError: Error {
+    case cannotConvertServerResponse
+}
 
 extension Todo {
-    class func list() {
-        AF.request(TodoRouter.get(nil)).responseJSON { (response) in
-            guard response.error == nil else {
-                print(response.error!)
-                return
-            }
-            
-            guard let todos = response.value as? [[String: Any]] else {
-                print("Cannot read the Todo list from the server.")
-                return
-            }
-            
-            todos.prefix(upTo: 10).reversed().forEach { (json) in
-                guard let todo = Todo(json: json) else {
-                    print("Cannot create a todo from the JSON.")
+    class func requestList(from router: TodoRouter) -> Observable<[[String: Any]]> {
+        // 创建一个Observable
+        return Observable.create { (observer) -> Disposable in
+            let request = AF.request(TodoRouter.get(nil)).responseJSON { (response) in
+                guard response.error == nil else {
+                    //print(response.error!)
+                    observer.onError(response.error!)
                     return
                 }
                 
-                // self.todoList.append(todo)
+                guard let todos = response.value as? [[String: Any]] else {
+                    //print("Cannot read the Todo list from the server.")
+                    observer.onError(GetTodoListError.cannotConvertServerResponse)
+                    return
+                }
+                
+                observer.onNext(todos)
+                observer.onCompleted()
             }
             
-            // TODO: update table view
+            return Disposables.create {
+                request.cancel()
+            }
         }
+        
     }
 }
--- Boxue Swift Note/RxSwift/RxOnlineTodo/RxOnlineTodo/TodoListViewController.swift
+++ Boxue Swift Note/RxSwift/RxOnlineTodo/RxOnlineTodo/TodoListViewController.swift
@@ -6,16 +6,34 @@
 //  Copyright © 2021 Bq. All rights reserved.
 //
 
-import UIKit
 import Alamofire
+import RxSwift
+import UIKit
 
 class TodoListViewController: UITableViewController {
-    
     var todoList: [Todo] = []
+    let bag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let todoId: Int? = nil
+        Observable.just(todoId)
+            // 生成Observable<TudoRouter>
+            .map { todoId in
+                TodoRouter.get(todoId)
+            }
+            // 网络请求得到todo list
+            .flatMap { route in
+                Todo.requestList(from: route)
+            }
+            // 订阅，更新UI
+            .subscribe(onNext: { todos in
+                self.todoList = todos.compactMap { Todo(json: $0) } // 这里不直接用map是为了去除其中的Optional
+                self.tableView.reloadData()
+            }, onError: { error in
+                print(error.localizedDescription)
+            }).disposed(by: bag)
     }
 
     // MARK: - Table view data source
@@ -30,7 +48,7 @@ class TodoListViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Todo", for: indexPath)
-        
+
         let todoItem = todoList[indexPath.row]
         let title = todoItem.title
         let richTitle = NSMutableAttributedString(string: title)
@@ -38,10 +56,10 @@ class TodoListViewController: UITableViewController {
             richTitle.setAttributes([.strikethroughStyle: 2], range: NSRange(location: 0, length: richTitle.length))
         }
         cell.textLabel?.attributedText = richTitle
-        
+
         return cell
     }
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
